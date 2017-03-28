5f1bb7e375924d37da7d14909b7a0952d4950c47
Michael Lake <mlake900@gmail.com>
2017-03-28 17:34:40 -0400

    setting email to hello

--- js/contact_me.js
+++ js/contact_me.js
@@ -29,7 +29,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/mlake@alleywayapps.com",
+                url: "//formspree.io/hello@alleywayapps.com",
                 type: "POST",
                 data: {
                     name: name,
