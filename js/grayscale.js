bd4b7c203265297bfd257495929d27e186e1bc6e
Michael Lake <mlake900@gmail.com>
2017-04-07 23:51:09 -0400

    fix for broken auto-scroll nav

--- js/grayscale.js
+++ js/grayscale.js
@@ -17,10 +17,13 @@ $(window).scroll(function() {
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+        var id = $anchor.attr('href');
+        if (id.indexOf("\/#") == 0){
+            $('html, body').stop().animate({
+                scrollTop: $(id.substr(1)).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        }
     });
 });
 
